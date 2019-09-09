4a651dac6a609d4956ab08dd09dbe6abb442162c
jelenatadic96 <46044160+jelenatadic96@users.noreply.github.com>
2019-09-09 03:48:05 +0200

    korisnik; com+servise

---
+++ frontAgent/src/app/component/korisnik/login/login.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import { Login } from 'app/model/korisnik/login';
+import { Korisnik } from 'app/model/korisnik/korisnik';
+import { AuthService } from 'app/service/korisnik/auth.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  user: Login = new Login();
+  loggedUser: Korisnik = new Korisnik();
+  constructor(private auth: AuthService, private router: Router/*, private userService: KorisnikService*/, private authService: AuthService) { 
+  }
+
+  ngOnInit() {
+    if(localStorage.getItem('token') != null){
+    //  this.loggedUser.mejl = this.authService.getUsername(localStorage.getItem('token'));
+      // let isAdmin = false;
+      // this.userService.findByEmail(this.loggedUser.email).subscribe(
+      //   s=>{
+      //     this.loggedUser = s;          
+      //     this.loggedUser.role.forEach(element =>
+      //       {
+
+              
+      //       if(element.roleName == "ROLE_ADMIN"){
+      //         isAdmin = true;
+      //           }
+      //         }
+      //       )
+      //       if(!isAdmin){
+      //         if(localStorage.getItem('reservation') != null){
+      //           let acc : AccommodationReservation = JSON.parse(localStorage.getItem('reservation'))
+
+      //           if(acc.roomDTO.id != null && acc.roomDTO.id != undefined){
+      //             this.router.navigate(['bookAccommodation/' + acc.roomDTO.id]);
+      //           }
+      //           else{
+      //             this.router.navigate(['address']);
+      //           }
+      //         }
+      //         else{
+      //           this.router.navigate(['address']);
+      //         }
+      //       }
+      //       else{
+      //         this.router.navigate(['users']);
+      //       }
+      //   }
+      // )
+
+    }
+  }
+
+  login(){
+  localStorage.removeItem('token');
+  this.auth.login(this.user.mejl, this.user.lozinka)
+      .subscribe(
+        res => 
+        {
+          console.log(res);
+          alert("evo ga")
+          localStorage.setItem('token', res);
+          //let role = this.auth.getRoles(res);
+
+          
+          // //samo da bi postavio ulogo naog korisnika
+          // this.userService.findByEmail(this.user.email).subscribe(
+          //   s => {
+          //     this.loggedUser = s;
+          //     window.location.reload();
+              
+          //   },
+          //   err =>{
+          //     console.log("err za pronalazenje po mailu");
+          //   } 
+          // );
+
+        },
+        err => {
+          alert("Wrong email or password");
+          console.log("usao kao error za logovanje");
+        }
+      );
+  }
+ 
+
+}
---
+++ frontAgent/src/app/component/korisnik/poruke/poruke.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from 'app/service/korisnik/auth.service';
+import { KorisnikService } from 'app/service/korisnik/korisnik.service';
+import { PorukeService } from 'app/service/korisnik/poruke.service';
+import { Poruka } from 'app/model/korisnik/poruka';
+import { Agent } from 'app/model/korisnik/agent';
+import { Korisnik } from 'app/model/korisnik/korisnik';
+
+@Component({
+  selector: 'app-poruke',
+  templateUrl: './poruke.component.html',
+  styleUrls: ['./poruke.component.css']
+})
+export class PorukeComponent implements OnInit {
+
+ 
+  inboxKorisnika: Korisnik[] = [];
+  chat: Poruka[] = [];
+  private sub: any;
+  loggedUser: Korisnik = new Korisnik();
+  newMessage: Poruka = new Poruka();
+  p:any;
+
+  constructor(private route: ActivatedRoute, private userService: KorisnikService, private router: Router, private authService: AuthService, private chatService: PorukeService) {
+    let res = localStorage.getItem('token');
+    if(res != null){
+      // this.loggedUser.mejl = this.authService.getUsername(res);
+      // //this.chat.messages = [];
+    }
+   }
+
+  ngOnInit() {
+    // this.userService.findByEmail(this.loggedUser.mejl).subscribe(
+    //   e => {
+    //     this.loggedUser = e;
+        //dobijem sve korisnike sa kojima je pricano
+        this.chatService.getInbox(this.loggedUser.id).subscribe(
+         s => {
+           this.inboxKorisnika = s;
+           this.inboxKorisnika.forEach(element => {
+             this.chatService.getMessages(this.loggedUser.id, element.id).subscribe( //dobavi sve poruke za tog korisnika
+               ss => {
+                 element.porukeSaAgentom = ss;
+               }
+             )
+           })
+         }
+         )
+      // })
+  }
+
+  loggedUsersMessage(message: Poruka){
+    if(message.agentDTO.id == this.loggedUser.id){
+      return true;
+    }
+    return false;
+  }
+
+  sendMessage(){
+    let d = new Date();
+    let datestring = d.getFullYear() + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" + ("0" + d.getDate()).slice(-2) 
+    this.newMessage.vreme = datestring;
+    this.newMessage.agentDTO.id = this.loggedUser.id; //proveriti za ovo posto je djuka pametan pa mu korinsik i agent nisu ista klasa
+    this.chat.forEach(element =>
+      {
+        if(element.korisnikDTO.id != this.loggedUser.id){
+          this.newMessage.korisnikDTO = element.korisnikDTO;
+        }
+      })                          //agent, user, poruka
+    this.chatService.sendMessage(this.newMessage.agentDTO.id, this.newMessage.korisnikDTO.id, this.newMessage).subscribe(
+      s => {
+        this.chat.push(this.newMessage);
+        this.newMessage = new Poruka();
+      }
+    )
+  }
+
+  changeActiveChat(chat:Korisnik){  //da promeni korisnika
+    this.chat = chat.porukeSaAgentom;
+    this.chatService.getMessages(this.loggedUser.id, chat.id).subscribe(
+      s => {
+        this.chat = s;
+      }
+    )
+    // chat.porukeSaAgentom.forEach(element =>
+    //   {
+    //     element.opened = true;
+    //   })
+   }
+
+}
